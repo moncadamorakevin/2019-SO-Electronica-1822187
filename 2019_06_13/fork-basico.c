c1a32592a7c1d0f9b5bd0f4ca2faebb1951fdc48
invitado@s5pc38 <invitado@s5pc38.eisc.univalle.edu.co>
2019-06-13 11:27:47 -0500

    Adicionando fork-basico.c

---
+++ 2019_06_13/fork-basico.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+int main(int argc, char** argv) {
+  pid_t pid;
+ 
+  printf("hola\n"); 
+  pid = fork(); printf("hola a todos\n");
+  if (pid != 0) {
+  printf("Soy el padre de mi id es %d y el de mi hijo es %d\n",getpid(),pid);
+  } else {
+    printf("Soy el hijo con id %d y mi variable pid es %d\n",getpid(),pid);
+}
+  
+  return 0;
+}
+
